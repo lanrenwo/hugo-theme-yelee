5575708efa478c650d4508ca35ea45d84426de3f
lanrenwo <phpcleps@gmail.com>
2019-11-19 10:04:03 +0800

    修改并应用于自己的blog

--- static/js/main.js
+++ static/js/main.js
@@ -154,8 +154,8 @@ require([], function () {
     //是否新窗口打开链接
     if (yiliaConfig.open_in_new == true) {
         $(".article a[href]").attr("target", "_blank")
+        $(".archive-article-title, .github-widget a").attr("target", "_blank");
     }
-    $(".archive-article-title, .github-widget a").attr("target", "_blank");
 
     //随机颜色
     var colorList = ["#6da336", "#ff945c", "#66CC66", "#99CC99", "#CC6666", "#76becc", "#c99979", "#918597", "#4d4d4d"];
